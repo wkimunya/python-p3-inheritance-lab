6fc8315a9ada957c7c8477d81a3b23cd2212e457
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-10-15 11:10:14 +0300

    completed lab

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,11 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, new_knowledge):
+        # Add the new knowledge to the student's self.knowledge list
+        self.knowledge.append(new_knowledge)
+    
